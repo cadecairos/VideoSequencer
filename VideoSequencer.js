51e5f4cb8b9c7d472f6b6611ddbb61ad6bdb2573
unknown <chris.decairos@gmail.com>
2010-10-15 17:39:03 -0400

    VideoSequencer.js skeleton

---
+++ VideoSequencer.js
@@ -0,0 +1,38 @@
+/* 
+** VideoSequencer.js
+** Created by: Christopher Decairos
+** Date: October 15, 2007
+*/
+
+var VideoSequencer = function() {
+  
+  /* Length of all segments */
+  var duration = 0;
+  
+  /* Current Time */
+  var currentTime = 0;
+
+  /* video objects */`
+  var segments = [];
+
+  /*
+  ** functions
+  */
+
+  this.play = function () {
+    
+  };
+
+  this.pause = function () {
+
+  };
+
+  this.add = function () {
+    
+  };
+    
+  this.seek = function () {
+    
+  };
+  
+};
