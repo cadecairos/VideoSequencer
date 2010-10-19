c8fa9479e906068c395934402b16338fc1f1f5b3
unknown <chris.decairos@gmail.com>
2010-10-19 12:32:05 -0400

    got video elemnts to be added to the sequencer and play consecutively.

--- VideoSequencer.js
+++ VideoSequencer.js
@@ -4,37 +4,69 @@
 ** Date: October 15, 2010
 */
 
-(function() {
-  
-  var VideoSequencer = this.VideoSequencer = {};
-  
-  /* Length of all segments */
-  this.duration = 0;
-  
-  /* Current Time */
-  this.currentTime = 0;
+(function () {
 
-  /* video objects */`
-  var segments = [];
+    var VideoSequencer = this.VideoSequencer = function (src) {
+        for (var i = 0; i < src.length; i++) {
+            vid = document.getElementById(src[i]);
+            vid.addEventListener("ended", swap, false);
+            segments.push(vid);
+        }
+        segments[0].setAttribute("style", "display: inline");
 
-  /*
-  ** functions
-  */
 
-  this.play = function () {
-    
-  };
+        /*
+        segments[0].addEventListener("onplay", ,false);
+        segments[0].addEventListener("onpause", , false); */
+        segments[0].play();
 
-  this.pause = function () {
+    };
 
-  };
+    /* Length of all segments */
+    this.duration = 0;
 
-  this.add = function () {
-    
-  };
-    
-  var seek = function () {
-   
-  };
-  
-}());
+    /* Current Time */
+    this.currentTime = 0;
+
+    /* video objects */
+    var segments = [];
+
+    /* is a video playing*/
+    var isPlaying;
+
+    /*
+    ** functions
+    */
+    var swap = function () {
+        for (var i = 0; i < segments.length; i++) {
+            if (segments[i].style.display === "inline") {
+                segments[i].setAttribute("style", "display: none");
+                if (segments[i + 1]) {
+                    segments[i + 1].setAttribute("style", "display: inline");
+                    segments[i + 1].play();
+                    break;
+                }
+            }
+        }
+    }
+
+    this.play = function () {
+
+    };
+
+    this.pause = function () {
+
+    };
+
+    this.add = function (vid) {
+        if (vid) {
+            this.segments[this.segments.length] = vid;
+
+        }
+    };
+
+    var seek = function () {
+
+    };
+
+} ());
