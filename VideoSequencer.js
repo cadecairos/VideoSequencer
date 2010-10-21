7ab74ff19af35e93ca8cb4e79079e8cbb3c4b1ab
unknown <chris.decairos@gmail.com>
2010-10-21 16:05:58 -0400

    got the constructor to find the total duration of all the videos

--- VideoSequencer.js
+++ VideoSequencer.js
@@ -7,17 +7,35 @@
 (function () {
 
     var VideoSequencer = this.VideoSequencer = function (src) {
+
+        var that = this;
         for (var i = 0; i < src.length; i++) {
             vid = document.getElementById(src[i]);
             vid.addEventListener("ended", swap, false);
             segments.push(vid);
         }
+
+        var getTotalDuration = setInterval(function () {
+            var metaDataIsLoaded = true;
+            for (var i = 0; i < segments.length; i++) {
+                //console.log("READY STATE: " + i + " : " + segments[i].readyState);
+                if (segments[i].readyState < 3) {
+                    metaDataIsLoaded = false;
+                }
+                //console.log(metaDataIsLoaded);
+            }
+            if (metaDataIsLoaded === true) {
+                that.duration = 0;
+                clearInterval(getTotalDuration);
+                for (var i = 0; i < segments.length; i++) {
+                    that.duration += segments[i].duration;
+                    //console.log(segments[i].duration);
+                }
+                //console.log(that.duration);
+            }
+        }, 50);
+
         segments[0].setAttribute("style", "display: inline");
-
-
-        /*
-        segments[0].addEventListener("onplay", ,false);
-        segments[0].addEventListener("onpause", , false); */
         segments[0].play();
 
     };
