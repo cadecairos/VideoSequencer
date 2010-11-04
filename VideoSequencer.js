65d1339fa6b54bfd6f556f316b0342054cb131f5
cadeciaros <chris.decairos@gmail.com>
2010-11-03 22:20:13 -0400

    Fixed braces and parentheses around functions and logical constructs, put parentheses on last line [#121]

--- VideoSequencer.js
+++ VideoSequencer.js
@@ -4,31 +4,31 @@
 ** Date: October 15, 2010
 */
 
-(function (){
+(function() {
 
-    var VideoSequencer = this.VideoSequencer = function (src){
+    var VideoSequencer = this.VideoSequencer = function(src) {
 
-        for (var i = 0; i < src.length; i++){
+        for (var i = 0; i < src.length; i++) {
             vid = document.getElementById(src[i]);
             vid.addEventListener("ended", swap, false);
             segments.push(vid);
         }
 
-        var getTotalDuration = setInterval(function (){
-            for (var i = 0; i < segments.length; i++){
-                if (isNaN(segments[i].duration) || segments[i].duration <= 0){
+        var getTotalDuration = setInterval(function() {
+            for (var i = 0; i < segments.length; i++) {
+                if (isNaN(segments[i].duration) || segments[i].duration <= 0) {
                     return;
                 }
             }
             duration = 0;
             clearInterval(getTotalDuration);
-            for (var i = 0; i < segments.length; i++){
+            for (var i = 0; i < segments.length; i++) {
                 duration += segments[i].duration;
             }
         }, 50);
 
         var submitBtn = document.getElementById("submit");
-        submitBtn.addEventListener("click", function (){
+        submitBtn.addEventListener("click", function() {
             var tb = document.getElementById("timeToSeek");
             return seek(toSeconds(tb.value));
         }, false);
@@ -53,11 +53,11 @@
     /*
     ** functions
     */
-    var swap = function (){
-        for (var i = 0; i < segments.length; i++){
-            if (segments[i].style.display === "inline"){
+    var swap = function() {
+        for (var i = 0; i < segments.length; i++) {
+            if (segments[i].style.display === "inline") {
                 segments[i].setAttribute("style", "display: none");
-                if (segments[i + 1]){
+                if (segments[i + 1]) {
                     segments[i + 1].setAttribute("style", "display: inline");
                     segments[i + 1].currentTime = 0;
                     segments[i + 1].play();
@@ -67,46 +67,47 @@
         }
     }
 
-    var swapTo = function (index, time){
-        for (var i = 0; i < segments.length; i++){
-            if (i === index){
+    var swapTo = function(index, time) {
+        for (var i = 0; i < segments.length; i++) {
+            if (i === index) {
                 segments[i].setAttribute("style", "display: inline");
                 segments[i].currentTime = time;
                 segments[i].play();
             }
-            else{
+            else {
                 segments[i].pause();
                 segments[i].setAttribute("style", "display: none");
             }
         }
     }
 
-    this.play = function (){
+    this.play = function() {
 
     };
 
-    this.pause = function (){
+    this.pause = function() {
 
     };
 
-    this.add = function (vid){
-        if (vid){
+    this.add = function(vid) {
+        if (vid) {
             this.segments[this.segments.length] = vid;
         }
     };
 
-    var seek = function (seconds){
-        if (seconds > duration || seconds < 0){ return }
+    var seek = function(seconds) {
+        if (seconds > duration || seconds < 0) { return; }
 
-        for (var i = 0; i < segments.length; i++){
-            if (segments[i].duration > seconds){
+        for (var i = 0; i < segments.length; i++) {
+            if (segments[i].duration > seconds) {
                 swapTo (i, seconds);
                 break;
-            } else{
+            } 
+	    else {
                 seconds -= segments[i].duration;
             }
         }
 
     };
 
-} ())
+} ());
