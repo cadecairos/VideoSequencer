d91352f05ca312c56c5817689bc699e479e1256a
cadeciaros <chris.decairos@gmail.com>
2010-11-16 23:00:39 -0500

    reworked the code entirely. data is now parsed from and  xml file. beginnings of video tag creation are in the works [#132]

--- VideoSequencer.js
+++ VideoSequencer.js
@@ -4,110 +4,139 @@
 ** Date: October 15, 2010
 */
 
-(function() {
+(function () {
 
-    var VideoSequencer = this.VideoSequencer = function(src) {
+	var VideoSequencer = this.VideoSequencer = function () {
+	
+		/* save reference to self */
+		var self = this;
+		
+		/* video tags */
+		this.playingVideo;
+		this.nextVideo;
+	    
+		/* video data */
+	    this.segments =[];
+	    
+	    /* Length of all segments */
+	    this.duration = 0;
 
-        for (var i = 0; i < src.length; i++) {
-            vid = document.getElementById(src[i]);
-            vid.addEventListener("ended", swap, false);
-            segments.push(vid);
-        }
+	    /* Current Time */
+	    this.currentTime = 0;
 
-        var getTotalDuration = setInterval(function() {
-            for (var i = 0; i < segments.length; i++) {
-                if (isNaN(segments[i].duration) || segments[i].duration <= 0) {
-                    return;
-                }
-            }
-            duration = 0;
-            clearInterval(getTotalDuration);
-            for (var i = 0; i < segments.length; i++) {
-                duration += segments[i].duration;
-            }
-        }, 50);
+	    /* Target Div for Video tags  */
+	    this.divTag;
+		
+		var swap = function () {
+			for (var i = 0; i < segments.length; i++) {
+				if (segments[i].style.display === "inline") {
+					segments[i].setAttribute("style", "display: none");
+					if (segments[i + 1]) {
+						segments[i + 1].setAttribute("style", "display: inline");
+						segments[i + 1].currentTime = 0;
+						segments[i + 1].play();
+						break;
+					}
+				}
+			}
+		};
+		
+		var swapTo = function (index, time) {
+			for (var i = 0; i < segments.length; i++) {
+				if (i === index) {
+					segments[i].setAttribute("style", "display: inline");
+					segments[i].currentTime = time;
+					segments[i].play();
+				}
+				else {
+					segments[i].pause();
+					segments[i].setAttribute("style", "display: none");
+				}
+			}
+		};
+		
+		var seek = function (seconds) {
+			if (seconds > duration || seconds < 0) { return; }
 
-        var submitBtn = document.getElementById("submit");
-        submitBtn.addEventListener("click", function() {
-            var tb = document.getElementById("timeToSeek");
-            return seek(toSeconds(tb.value));
-        }, false);
+			for (var i = 0; i < segments.length; i++) {
+				if (segments[i].duration > seconds) {
+					swapTo(i, seconds);
+					break;
+				}
+				else {
+					seconds -= segments[i].duration;
+				}
+			}
+		};
+		
+		this.playingVideo = document.getElementById ("sequencer");
+		this.convertXML(this.getSegmentData(this.playingVideo.getAttribute('segment-data')).responseXML);
+        this.init();
+    };
 
-        segments[0].setAttribute("style", "display: inline");
-        segments[0].play();
+	VideoSequencer.prototype.init = function () {
+		this.videoDiv = document.getElementById("videoDiv");
+		this.playingVideo = document.getElementById('sequencer');
+		this.segDta = this.playingVideo.getAttribute("segment-data");
+		this.videoDiv.removeChild(this.playingVideo);
+		delete this.playingVideo;
+		this.playingVideo = document.createElement("video");
+		this.playingVideo.setAttribute("src", this.segments[0].src);
+		this.playingVideo.setAttribute("width", "250px")
+		this.playingVideo.setAttribute("segment-data", this.segDta);
+		this.videoDiv.appendChild(this.playingVideo);
+		};
+		
+	VideoSequencer.prototype.getSegmentData = function(segDataFile) {
+		var xhttp = new XMLHttpRequest();
+		if (xhttp) {
+			xhttp.open("GET", segDataFile, false);
+			xhttp.send();
+			return xhttp;
+		} else {
+			return false;
+		}
+	};
+	
+	VideoSequencer.prototype.convertXML = function (xmlDoc){
+		var self = this;
+		var parseNode = function (node) {
+			var childNodes = node.childNodes;
+			var aSource = {};
+			for (var i = 0; i < childNodes.length; i++) {
+				var child = childNodes[i];
+				if (child.nodeType === 1 ) {
+					if (child.nodeName === "source") {
+						aSource.src = child.textContent;
+					} else if (child.nodeName === "length") {
+						aSource.length = child.textContent;
+					}
+				}
+			}
+			self.segments.push(aSource);
+		}
+		
+		var xml = xmlDoc.documentElement.childNodes;
+		for (var i = 0, xmlLen = xml.length; i < xmlLen; i++) {
+			if (xml[i].nodeType === 1){
+				if (xml[i].nodeName === "vid"){
+					parseNode(xml[i]);
+				}
+			}
+		}	
+	};
+	
+    VideoSequencer.prototype.play = function () {
 
     };
 
-    /* Length of all segments */
-    this.duration = 0;
-
-    /* Current Time */
-    this.currentTime = 0;
-
-    /* video objects */
-    var segments = [];
-
-    /* the currently playing video */
-    var currentPlayingVideo;
-
-    /*
-    ** functions
-    */
-    var swap = function() {
-        for (var i = 0; i < segments.length; i++) {
-            if (segments[i].style.display === "inline") {
-                segments[i].setAttribute("style", "display: none");
-                if (segments[i + 1]) {
-                    segments[i + 1].setAttribute("style", "display: inline");
-                    segments[i + 1].currentTime = 0;
-                    segments[i + 1].play();
-                    break;
-                }
-            }
-        }
-    }
-
-    var swapTo = function(index, time) {
-        for (var i = 0; i < segments.length; i++) {
-            if (i === index) {
-                segments[i].setAttribute("style", "display: inline");
-                segments[i].currentTime = time;
-                segments[i].play();
-            }
-            else {
-                segments[i].pause();
-                segments[i].setAttribute("style", "display: none");
-            }
-        }
-    }
-
-    this.play = function() {
+    VideoSequencer.prototype.pause = function () {
 
     };
 
-    this.pause = function() {
-
+    VideoSequencer.prototype.add = function (vid) {
+        // if (vid) {
+            // this.segments[this.segments.length] = vid;
+        // }
     };
-
-    this.add = function(vid) {
-        if (vid) {
-            this.segments[this.segments.length] = vid;
-        }
-    };
-
-    var seek = function(seconds) {
-        if (seconds > duration || seconds < 0) { return; }
-
-        for (var i = 0; i < segments.length; i++) {
-            if (segments[i].duration > seconds) {
-                swapTo (i, seconds);
-                break;
-            } 
-	    else {
-                seconds -= segments[i].duration;
-            }
-        }
-
-    };
-
 } ());
