fa25760ee03a39ecb47bf4ce3a333f5de69ef480
cadeciaros <chris.decairos@gmail.com>
2010-12-07 23:48:08 -0500

    using soda.js as a base, Ive added a timebar ui to track progress. hasa pause and play button. needs seek functionality still. [#202]

--- VideoSequencer.js
+++ VideoSequencer.js
@@ -34,9 +34,115 @@
     this.divTag = document.getElementById("videoDiv");
     this.divTag.removeChild(this.playingVideo);
  
-    this.playingVideo = this.createVideoTag(this.width,this.height,true, false);
+    this.playingVideo = this.createVideoTag(this.width,this.height,false, false);
     this.addEventListener("timeupdate", function() { return self.update(); });
-    this.nextVideo = self.createVideoTag(this.width,this.height,true, true);
+    this.nextVideo = self.createVideoTag(this.width,this.height,false, true);
+
+    /* Time bar div */
+    this.timeDiv = { tag : document.getElementById("controlDiv"),
+                     width: document.getElementById("controlDiv").getAttribute("width"),
+                     height: document.getElementById("controlDiv").getAttribute("height") };
+    
+    /* Time bar (from soda.js) */
+    this.timeBar = new (function (seq) {
+
+      var parent = seq;
+      var state = "paused";
+      var totalDuration = parent.duration;
+      var duration = parent.duration;
+      var buffer;
+      var bar;
+      var pauseButton   = this.pauseButton   = document.createElement("button");
+      var playButton    = this.playButton    = document.createElement("button");
+      var buffer        = this.buffer        = document.createElement("div");
+      var ellapsedTime  = this.ellapsedTime  = document.createElement("div");
+      var totalDuration = this.totalDuration = document.createElement("span");
+
+    
+      var pause = function () {
+        parent.pause()
+        playButton.style.display = "inline";
+        pauseButton.style.display = "none";
+      };
+      
+      var play = function (){
+        parent.play()
+        pauseButton.style.display = "inline";
+        playButton.style.display = "none";
+      };
+      var updateSeekable = function(){
+        var endVal = parent.playingVideo.seekable && parent.playingVideo.seekable.length ? parent.playingVideo.seekable.end() : 0;
+        //FF has a buffered event
+        /*if (endVal === 0) {
+          videoElement.buffered;
+          for (var i=0; i<r.length; i++) {
+          {
+          }
+        }*/
+        buffer.style.width = (100 / (duration || 1) * endVal) + '%';
+      }
+      var updateEllapsedTime = function(){
+        var totalTimeElapsed = 0;
+        var segments = seq.segments;
+        for (var i = 0, len = segments.length; i < seq.currentIndex; i++)
+        {
+          totalTimeElapsed += parseInt(segments[i].length);
+        }
+        totalTimeElapsed += parent.playingVideo.currentTime;
+        var c = totalDuration.style.width.split("p")[0];
+        ellapsedTime.style.left = ( ( totalTimeElapsed / duration  ) * parseInt(c)) +"px";
+      }
+      this.draw = function() {
+        seq.addEventListener('timeupdate', updateEllapsedTime, false);
+        //buffer bar for progress
+        seq.addEventListener('progress', updateSeekable, false);
+        //Firefox in its video element that causes the video.seekable.end() value not to be the same as the duration. 
+        //To work around this issue, we can also listen for the durationchange event
+        seq.addEventListener('durationchange', updateSeekable, false);
+    
+        playButton.title = "play";
+        playButton.value = "play";
+        playButton.innerHTML = "&#x25BA;";
+        playButton.style.display = "inline";
+        playButton.style.width = "45px";
+        playButton.onclick = play;
+    
+        pauseButton.title = "pause";
+        pauseButton.value  = "pause";
+        pauseButton.innerHTML  = "&#x2590;&#x2590;";
+        pauseButton.style.display = "inline";
+        pauseButton.style.width = "45px";
+        pauseButton.onclick = pause;
+    
+        buffer.title = "Preloaded";
+        buffer.setAttribute("style","width:0px;height:15px; position:absolute; -moz-opacity: 0.5; opacity: 0.5; background-color: grey;");
+         
+        ellapsedTime.title = "Ellapsed Time";
+        ellapsedTime.setAttribute("style","width:3px;height:15px; position:absolute; background-color: black ;");
+
+        totalDuration.title ="total";
+        totalDuration.setAttribute("style","width:"+ (seq.timeDiv.width - 50) +"px;height:15px; position:absolute; border-style:solid;border-width:3px;position: absolute; border-radius:10px; -moz-border-radius: 10px;-webkit-border-radius: 10px;");
+    
+        //add buffered and ellapsedTime to the total duration div so they stay on top of each other
+        totalDuration.appendChild(buffer);
+        totalDuration.appendChild(ellapsedTime);
+        //put the bars in a separate div
+        var barContainer = document.createElement("span");
+        barContainer.appendChild(totalDuration);
+        
+        var container = document.createElement("span");
+        container.style.width="100%";
+        container.appendChild(playButton);
+        container.appendChild(pauseButton);
+        container.appendChild(barContainer);
+      
+        seq.timeDiv.tag.appendChild(container);
+        play();
+      }   
+    }
+    )(this);
+    //this.timeBar = new Timebar(this);
+    this.timeBar.draw();
   };
 
   VideoSequencer.prototype.calculateDuration = function() {
@@ -54,12 +160,11 @@
       this.playingVideo.setAttribute("style", "display: inline");
       this.playingVideo.setAttribute("id", "active");
     } else {
-      this.playingVideo = this.createVideoTag(this.width, this.height, true, false);
+      this.playingVideo = this.createVideoTag(this.width, this.height, false, false);
 	  this.divTag.removeChild(this.nextVideo);
     }
-    this.addListenersToCurrentVideo("timeupdate");
-    //this.playingVideo.play();		
-    this.nextVideo = this.createVideoTag(this.width,this.height,true, true);
+    this.addListenersToCurrentVideo("timeupdate");		
+    this.nextVideo = this.createVideoTag(this.width,this.height,false, true);
   }
 
   VideoSequencer.prototype.getSegmentData = function(segDataFile) {
@@ -154,9 +259,10 @@
     newSource.length = len || 0;
     var l = this.segments.length;
     this.segments.splice((len >= 0 && len < l ? len : l),0,{ src: src || "" , length : len || 0 });
+	this.duration = calculateDuration();
   };
 
-  VideoSequencer.prototype.addEventListener = function(event, callback) {
+  VideoSequencer.prototype.addEventListener = function(event, callback, useCapture) {
     var callbackExists = false;
     var queue = this.events[event] = this.events[event] || [];
     for (var i = 0, l = queue.length; i < l; i++) {
@@ -167,7 +273,7 @@
     }
     if (!callbackExists) { 
       queue.push(callback);
-      this.playingVideo.addEventListener(event, callback, false); 
+      this.playingVideo.addEventListener(event, callback, useCapture); 
     }
   };
   
@@ -198,27 +304,26 @@
     if (!(time >= 0 && time <= duration)) { return; }
     segments = this.segments;
     for (var i = 0, l = segments.length; i < l; i++) {
-        segLen = segments[i].length;
-        if (time > segLen) {
-          time = time - segLen;
-          continue;
+      segLen = segments[i].length;
+      if (time > segLen) {
+        time = time - segLen;
+        continue;
+      }
+      if (!(this.currentIndex == i)) {
+            this.currentIndex = i;
+            this.swap(true);
+          } 
+      if (!this.playingVideo.paused) this.pause();
+      
+      var seekIntoVid = function (t) {
+        if (self.playingVideo.readyState >= 2) {
+          self.playingVideo.currentTime = t;
+          self.play();
+          clearInterval(seekWhenReady);
         }
-		if (!(this.currentIndex == i)) {
-          this.currentIndex = i;
-          this.swap(true);
-        } 
-		if (!this.playingVideo.paused) this.pause();
-		
-		 var seekIntoVid = function (t) {
-		  if (self.playingVideo.readyState >= 2) {
-            self.playingVideo.currentTime = t;
-            self.play();
-            clearInterval(seekWhenReady);
-          }
-        };
-		
-        var seekWhenReady = setInterval( function(){ seekIntoVid(time)} ,50);
-		break;
+      };
+      var seekWhenReady = setInterval( function(){ seekIntoVid(time)} ,50);
+      break;
     }
   };	
 } ());
